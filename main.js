eef0bbac15add9f354d6382bd04ea5d9abe9a73b
zhangchile <zzlchile@gmail.com>
2014-11-28 15:25:07 +0800

    功能完成，测试通过。

--- main.js
+++ main.js
@@ -43,10 +43,13 @@ var CreateSet = function(name) {
 *   更新点的属性
 */
 var UpdatePoint = function(point, attr, value) {
-    if(attr != 'N_EPS') {
-        point.attr = value;
-    } else {
+    // console.log(point);
+    if(attr == 'type') {
+        point.type = value;
+    } else if(attr == 'N_EPS'){
         point.N_EPS.push(value);
+    } else if(attr == 'IN_SET') {
+        point.IN_SET = value
     }
 
 }
@@ -65,12 +68,16 @@ var CalNEPS = function(point) {
 
     for(var i = 0, n = point_set.length; i < n; i++) {
         var p = point_set[i];
+        if(p.x == point.x && p.y == point.y) continue;
         var dist = CalDistance(point, p);
+        // console.log(dist);
         if( dist <= _EPS ) {
+            // console.log("add a point to ("+point.x+","+point.y+")");
             // console.log(p);
             UpdatePoint(point, "N_EPS", p);
         }
     }
+    // console.log(point.N_EPS);
     return point;
 }
 
@@ -101,6 +108,33 @@ var PrintPoint = function(point) {
 }
 
 /**
+*   打点的坐标
+*/
+var PrintSets = function(set) {
+    if(set instanceof Array) {
+        for(var i = 0, n = set.length; i < n; i++) {
+            s = set[i];
+            console.log("name: " + s.name + ", points:" + s.points.length);
+            PrintPoint(s.points);
+        }
+    } else {
+        console.log("name: " + set.name + ", points:" + set.points.length);
+        PrintPoint(set.points);
+    }
+}
+
+/**
+*   随机名字
+*/
+var getName = function() {
+    var str = String.fromCharCode(Math.floor( 65 + Math.random() * (25 + 1)))
+            + String.fromCharCode(Math.floor( 65 + Math.random() * (25 + 1)))
+            + String.fromCharCode(Math.floor( 65 + Math.random() * (25 + 1)))
+            + String.fromCharCode(Math.floor( 65 + Math.random() * (25 + 1)));
+    return str;
+}
+
+/**
 *   算法入口
 */
 var DBSCAN = function() {
@@ -109,20 +143,24 @@ var DBSCAN = function() {
         var p = point_set[i];
         if(p.IN_SET == true) continue;//该点已经在簇中
         //计算该点的NEPS
-        CalNEPS(p);
+        // console.log("first");
+        p = CalNEPS(p);
+        // console.log(p.N_EPS);
         //核心点
+
         if(p.N_EPS.length >= _MinPts) {
             // console.log("N_EPS.length:"+p.N_EPS.length);
             // console.log(p.N_EPS);
             // console.log(p.x+ " " +p.y);
-            var name = Date.now();//用时间戳来定义簇名
+            var name = getName();//用时间戳来定义簇名
             var new_set = CreateSet(name);//新建个簇
+            // console.log(p);
             UpdatePoint(p, "type", _CORE);//设置该点为核心对象
             AddPoint(p, new_set);//把核心点加入簇中
             UpdatePoint(p, "IN_SET", true);
             for (var j = p.N_EPS.length - 1; j >= 0; j--) {
                 var N_P = p.N_EPS[j];
-                CalNEPS(N_P);
+                N_P = CalNEPS(N_P);
                 if(N_P.N_EPS.length >= _MinPts) {//领域中的点也是核心对象，则并入到这个簇中
                     UpdatePoint(N_P, "type", _CORE);//设置该点为核心对象
                     AddPoint(N_P, new_set);//把核心点加入簇中
@@ -133,20 +171,25 @@ var DBSCAN = function() {
             };
         } else {
             p.N_EPS = [];
-            UpdatePoint(N_P, "type", _NOISE_BORDER);//设置该点为边界对象
+            UpdatePoint(p, "type", _NOISE_BORDER);//设置该点为边界对象
         }
     }
 }
 
 CreatePoint(1,2);
 CreatePoint(2,2);
-CreatePoint(2,1);
+CreatePoint(1,1);
 
-// CreatePoint(5,6);
-// CreatePoint(5,5);
-// CreatePoint(5,7);
+// PrintPoint(point_set);
+CreatePoint(5,6);
+CreatePoint(5,5);
+CreatePoint(5,7);
 
+CreatePoint(9,9);
+CreatePoint(10,10);
+CreatePoint(10,11);
+CreatePoint(13,10);
 
 DBSCAN();
-PrintPoint(point_set);
-console.log(sets);
+PrintSets(sets);
+// PrintPoint(point_set);
